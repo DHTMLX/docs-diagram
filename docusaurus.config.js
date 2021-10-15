5b9dcf1a643bc8c9e4398612f86566d9350306f9
Victoriya <vashkinazi@xbsoftware.by>
2021-10-15 14:58:21 +0300

    [add] api key for search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -164,8 +164,8 @@ module.exports = {
         algolia: {
             // This is a read-only, search-only key served directly by the front-end, managed by Algolia via their
             // free DocSearch program. The key is not sensitive. See https://docsearch.algolia.com/ for more details.
-            apiKey: '123',
-            indexName: 'diagram',
+            apiKey: '027f495bbecd54a44f3d5bf4f842e611',
+            indexName: 'diagramalt',
 
             // Optional: see doc section bellow
             contextualSearch: true,
